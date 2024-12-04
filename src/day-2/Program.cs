1738c685fa1171eefec8a48a8899c28cd32e8ee2
Jordan Taylor <jordantay9014@outlook.com>
2024-12-03 20:06:22 -0600

    feat: day 2

---
+++ src/day-2/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+string filePath = @"input.txt";
+//string[] lines = File.ReadAllLines(path);
+
+List<int[]> data = new List<int[]>();
+
+void Aoc(string filePath)
+{
+    using (StreamReader sr = new StreamReader(filePath))
+    {
+        string? line;
+        int safereports = 0;
+        while ((line = sr.ReadLine()) != null)
+        {
+            int[] row = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            data.Add(row);
+
+            if (IsSafe(row))
+            {
+                safereports++;
+            }
+        }
+
+        Console.WriteLine($"Total safe reports: {safereports}");
+    }
+}
+
+bool IsSafe(int[] levels)
+{
+    bool increasing = levels[0] < levels[^1];
+    for (int i = 0; i < levels.Length - 1; i++)
+    {
+        int diff = levels[i + 1] - levels[i];
+        Console.WriteLine(diff);
+        Console.WriteLine($"Abs: {Math.Abs(diff)}");
+        if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3 || (increasing && diff <= 0) || (!increasing && diff >= 0))
+        {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+Aoc(filePath);
